e4be81e0b2905b321436af27d40c67f67b1a1e6b
Laurie <laurie@farragar.com>
2016-08-08 21:19:31 +0100

    Use proper accel library

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ ugfx.text(5, 5, 'Hello world!', ugfx.RED)
 accel = pyb.Accel()
 
 while(True):
-    x = accel.x()
-    y = accel.y()
-    z = accel.z()
-    ugfx.text(5, 5, str(x), ugfx.RED)
+    accel = imu.get_acceleration()
+    x, y, z [accel[axis] for axis in ('x','y','z')] 
+    ugfx.text(5 5, str(x), ugfx.RED)
     ugfx.text(100, 5, str(y), ugfx.GREEN)
     ugfx.text(200, 5, str(z), ugfx.BLUE)
