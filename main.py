3500382fb30775d6a24e07408f5f02a3a6c89f36
Laurie <laurie@farragar.com>
2016-08-08 21:25:55 +0100

    Add requests logic

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import ugfx
 import buttons
 import pyb
+from http_client import get
 
 ugfx.init()
 ugfx.text(5, 5, 'Hello world!', ugfx.RED)
 accel = pyb.Accel()
+
+host = 'http://www.farragar.com'
+port = 80
+
 
 while(True):
     accel = imu.get_acceleration()
@@ -12,3 +17,7 @@ while(True):
     ugfx.text(5 5, str(x), ugfx.RED)
     ugfx.text(100, 5, str(y), ugfx.GREEN)
     ugfx.text(200, 5, str(z), ugfx.BLUE)
+    print(x, y, z)
+
+    if(x > TILT_THRESHOLD):
+    	get('{}:{}'.format(host, pot))
